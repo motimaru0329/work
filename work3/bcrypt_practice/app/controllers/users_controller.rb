905d08fc0b21b3d8d3d15841a4e880d1b11e2c24
motimaru0329 <oca2402it0002@edu.oca.ac.jp>
2025-10-10 06:11:52 +0000

    1010

---
+++ work3/bcrypt_practice/app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :require_login, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to mypage_path, notice: "サインアップしました！"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def require_login
+    redirect_to root_path, alert: "ログインしてください" unless logged_in?
+  end
+end
